36107c69a82cc361ec6eba2efed8d19af4a39576
Ricardo Ceballos <rickceballos@hotmail.com.ar>
2023-07-08 17:16:59 -0300

    zafirogames3.0

---
+++ main.js
@@ -0,0 +1,14 @@
+// navbar funcion scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    const logo = document.querySelector('#logo');
+    const scrollThreshold = 110;
+
+    if (window.scrollY > scrollThreshold) {
+        navbar.classList.add('navbar-scroll');
+        logo.classList.add('logo-scroll');
+    } else {
+        navbar.classList.remove('navbar-scroll');
+        logo.classList.remove('logo-scroll');
+    }
+});
